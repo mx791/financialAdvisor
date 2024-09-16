288c1eb0d0d2e195e1a7b0c8b20dc4883768b377
Charles <charles.laurioz@gmail.com>
2024-09-17 00:03:57 +0200

    maj menu

---
+++ web/src/components/Menu.tsx
@@ -0,0 +1,13 @@
+import React, { FC, ReactElement } from 'react';
+
+const Menu: FC = ({}): ReactElement => {
+    return (<div className='container menu'>
+        <div className='menu-item'>LOGO</div>
+        <div className='menu-item'>Accueil</div>
+        <div className='menu-item'>Exploration de titres</div>
+        <div className='menu-item'>Composition de portefeuille</div>
+        <div className='menu-item'>Suivi de portefeuille</div>
+    </div>)
+};
+
+export default Menu;
