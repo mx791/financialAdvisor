5c0d9d2504e0bab8d6414450f54d4b83c91ad022
Charles Lrz <56536939+mx791@users.noreply.github.com>
2024-09-17 13:11:12 +0000

    Navigation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Menu from './components/Menu';
 import LandingPage from './pages/LandingPage';
+import NonePage from './pages/NonePage';
 
 function App() {
+
+  const [page, setPage] = React.useState("MAIN");
+  const changePage = (i: String) => setPage(i + "");
+
   return (
     <div className="App">
-      <Menu />
-      <LandingPage />
+      <Menu setActivePage={changePage}/>
+      { page === "MAIN" ? (<LandingPage />) : "" }
+      { page !== "MAIN" ? (<NonePage />) : "" }
     </div>
   );
 }
--- web/src/components/Menu.tsx
+++ web/src/components/Menu.tsx
@@ -1,11 +1,15 @@
 import React, { FC, ReactElement } from 'react';
 
-const Menu: FC = (): ReactElement => {
+interface MenuProps {
+    setActivePage: (i: String) => void
+}
+
+const Menu: FC<MenuProps> = (props: MenuProps): ReactElement => {
     return (<div className='container menu'>
-        <div className='menu-item'>LOGO</div>
-        <div className='menu-item'>Exploration de titres</div>
-        <div className='menu-item'>Composition de portefeuille</div>
-        <div className='menu-item'>Suivi de portefeuille</div>
+        <div className='menu-item' onClick={() => props.setActivePage("MAIN")}>LOGO</div>
+        <div className='menu-item' onClick={() => props.setActivePage("EXPLORE")}>Exploration de titres</div>
+        <div className='menu-item' onClick={() => props.setActivePage("COMPOSE")}>Composition de portefeuille</div>
+        <div className='menu-item' onClick={() => props.setActivePage("FOLLOW")}>Suivi de portefeuille</div>
     </div>)
 };
 
