1d427215e06af8b782e0513f0a5d74cffbea8336
Charles Lrz <56536939+mx791@users.noreply.github.com>
2024-09-17 09:01:10 +0000

    maj readme

--- web/src/components/Menu.tsx
+++ web/src/components/Menu.tsx
@@ -3,7 +3,6 @@ import React, { FC, ReactElement } from 'react';
 const Menu: FC = (): ReactElement => {
     return (<div className='container menu'>
         <div className='menu-item'>LOGO</div>
-        <div className='menu-item'>Accueil</div>
         <div className='menu-item'>Exploration de titres</div>
         <div className='menu-item'>Composition de portefeuille</div>
         <div className='menu-item'>Suivi de portefeuille</div>
--- web/src/pages/LandingPage.tsx
+++ web/src/pages/LandingPage.tsx
@@ -2,8 +2,9 @@ import React, { FC, ReactElement } from 'react';
 
 const LandingPage: FC = (): ReactElement => {
     return (<div>
+        <div className='space'></div>
         <div className='container'>
-            <h1>Professionaliser la gestion de votre épargne</h1>
+            <h1>Professionalisez la gestion de votre épargne</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     </div>)
